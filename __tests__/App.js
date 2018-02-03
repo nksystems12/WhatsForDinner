306980746104ad8b0232621a4812f0a5cc749866
nkshah2 <nemishah1212@gmail.com>
2018-02-03 12:32:28 +0530

    Add eslint with basic rule config

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,12 +1,14 @@
+/* eslint-disable */
 import 'react-native';
 import React from 'react';
-import App from '../App';
-
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import App from '../App';
 
-it('renders correctly', () => {
+
+it( 'renders correctly', () => {
   const tree = renderer.create(
     <App />
   );
-});
+} );
+/* eslint-enable */
